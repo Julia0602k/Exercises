a58dbde43dbfb37ef90915b17d244e5b2049e1a1
Julia0602k <julia0602k@gmail.com>
2022-09-25 17:36:30 +0300

    3_4 print

--- 3_4.py
+++ 3_4.py
@@ -2,3 +2,15 @@ number1 = int(input('Write a first number:'))
 number2 = int(input('Write a second number:'))
 number3 = int(input('Write a third number:'))
 
+answer_negative = int(0)
+if number1 < 0:
+    answer_negative = answer_negative + 1
+if number2 < 0:
+    answer_negative = answer_negative + 1
+if number3 < 0:
+    answer_negative = answer_negative + 1
+answer_positive = 3 - answer_negative
+
+print('Answer is:\n')
+print('\tpositive:', answer_positive)
+print('\tnegative:', answer_negative)
