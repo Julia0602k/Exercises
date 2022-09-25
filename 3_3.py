3e184b7561be52e6830a237e4f66cb9ab47442a6
Julia0602k <julia0602k@gmail.com>
2022-09-25 16:48:13 +0300

    3_3 (var1,2,3)

--- 3_3.py
+++ 3_3.py
@@ -8,6 +8,16 @@ text2 = 'Your age are %s.' % (age)
 text3 = 'You live in %s.' % (city)
 print(text1, text2, text3)
 
+#variant2
+text_var2 = 'Hello, {}! Your age are {}. Your live in {}.'.format(name, age, city)
+print(text_var2)
+
+#variant3
+text_var3 = f'Hello, {name}! Your age are {age}. Your live in {city}.'
+print(text_var3)
+
+
+
 
 
 
