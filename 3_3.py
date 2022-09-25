9fe459dbbe7518a54701fe479db39bf0c9d3cdec
Julia0602k <julia0602k@gmail.com>
2022-09-25 16:42:05 +0300

    3_3 (var1)

---
+++ 3_3.py
@@ -0,0 +1,13 @@
+name = input('What is you name?')
+age = input('How old are you?')
+city = input('Where are you from?')
+
+#variant1
+text1 = 'Hello, %s!' % (name)
+text2 = 'Your age are %s.' % (age)
+text3 = 'You live in %s.' % (city)
+print(text1, text2, text3)
+
+
+
+
