9a487f7765e055ffba3c0c3fdf2b2b54883ff0dd
Julia0602k <julia0602k@gmail.com>
2022-09-25 18:17:22 +0300

    3_4 print variant2 startswith

--- 3_4.py
+++ 3_4.py
@@ -1,21 +1,15 @@
-number1 = bool(input('Write a first number:'))
-number2 = bool(input('Write a second number:'))
-number3 = bool(input('Write a third number:'))
+number1 = str(input('Write a first number:'))
+number2 = str(input('Write a second number:'))
+number3 = str(input('Write a third number:'))
+number1.startswith(-)
+number2.startswith(-)
+number3.startswith(-)
+
+summa_negative = int()
 summa_positive = int()
-summa_positive = number1 + number2 + number3
-summa_negative = 3 - summa_positive
+summa_negative = number1.startswith( - ) + number2.startswith(-) + number3.startswith(-)
+summa_positive = 3 - summa_negative
 print('Answer is:\n', '\tpositive:', summa_positive, '\n\tnegative:', summa_negative)
 
 
 
-
-#Variant 2
-answer_negative = int(0)
-if number1 < 0:
-    answer_negative = answer_negative + 1
-if number2 < 0:
-    answer_negative = answer_negative + 1
-if number3 < 0:
-    answer_negative = answer_negative + 1
-answer_positive = 3 - answer_negative
-print('Answer is:\n', '\tpositive:', answer_positive, '\n\tnegative:', answer_negative)
