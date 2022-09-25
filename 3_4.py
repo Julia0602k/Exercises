f86efffefc65d7564cc043d520addb64af84ac46
Julia0602k <julia0602k@gmail.com>
2022-09-25 18:01:12 +0300

    3_4 print all edit variant2

--- 3_4.py
+++ 3_4.py
@@ -1,7 +1,15 @@
-number1 = int(input('Write a first number:'))
-number2 = int(input('Write a second number:'))
-number3 = int(input('Write a third number:'))
+number1 = bool(input('Write a first number:'))
+number2 = bool(input('Write a second number:'))
+number3 = bool(input('Write a third number:'))
+summa_positive = int()
+summa_positive = number1 + number2 + number3
+summa_negative = 3 - summa_positive
+print('Answer is:\n', '\tpositive:', summa_positive, '\n\tnegative:', summa_negative)
 
+
+
+
+#Variant 2
 answer_negative = int(0)
 if number1 < 0:
     answer_negative = answer_negative + 1
@@ -10,5 +18,4 @@ if number2 < 0:
 if number3 < 0:
     answer_negative = answer_negative + 1
 answer_positive = 3 - answer_negative
-
 print('Answer is:\n', '\tpositive:', answer_positive, '\n\tnegative:', answer_negative)
