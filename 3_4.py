32fdbeba06334a2a3c8fc6c8614f5236eaf9819f
Julia0602k <julia0602k@gmail.com>
2022-09-25 18:35:22 +0300

    3_4 print variant2 startswith answer edit

--- 3_4.py
+++ 3_4.py
@@ -2,8 +2,6 @@ number1 = str(input('Write a first number:'))
 number2 = str(input('Write a second number:'))
 number3 = str(input('Write a third number:'))
 
-summa_negative = int()
-summa_positive = int()
 summa_negative = number1.startswith('-') + number2.startswith('-') + number3.startswith('-')
 summa_positive = 3 - summa_negative
 print('Answer is:\n', '\tpositive:', summa_positive, '\n\tnegative:', summa_negative)
