4d8094fb5e6c76f70357c850d467e4d9e3ad8eba
Julia0602k <julia0602k@gmail.com>
2022-09-25 18:45:23 +0300

    3_4 print 2 varianta

--- 3_4.py
+++ 3_4.py
@@ -6,5 +6,13 @@ summa_negative = number1.startswith('-') + number2.startswith('-') + number3.sta
 summa_positive = 3 - summa_negative
 print('Answer is:\n', '\tpositive:', summa_positive, '\n\tnegative:', summa_negative)
 
+# Variant 2
+number1 = int()
+number2 = int()
+number3 = int()
+summa_positive1 = (number1 > 0) + (number2 > 0) + (number3 > 0)
+summa_negative1 = 3 - summa_positive1
+print('Answer is:\n', '\tpositive:', summa_positive1, '\n\tnegative:', summa_negative1)
+
 
 
