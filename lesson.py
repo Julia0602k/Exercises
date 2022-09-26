2c2e672039e3e8a70e501711d519083f66e934fa
Julia0602k <julia0602k@gmail.com>
2022-09-26 11:54:18 +0300

    task1

--- lesson.py
+++ lesson.py
@@ -1,6 +1,7 @@
-n = int(input('Write N'))
+n = int(input('Write number N: '))
 numbers = list()
-for i in range(1, n):
+for i in range(1, n + 1):
     if not i % 2:
         numbers.append(i ** 2)
 print(numbers)
+
