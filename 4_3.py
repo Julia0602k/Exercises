2c6adfa5e66b741957fdd186cba549c185724723
Julia0602k <julia0602k@gmail.com>
2022-09-28 23:53:17 +0300

    task4_3 ready

--- 4_3.py
+++ 4_3.py
@@ -1,3 +1,7 @@
 #Заполнить словарь, где ключами будут выступать числа от 1 до n,
-# а значениями вложенный словарь с ключами 'name' и 'email', а значения для ключей - с клавиатуры
-n = int(input())
+# а значениями вложенный словарь с ключами 'name' и 'email', значения для ключей - с клавиатуры
+n = int(input('Write n: '))
+data = {
+    i: dict(name=input('Write your name'), email=input('Write your email'))
+    for i in range(n)}
+print(data)
