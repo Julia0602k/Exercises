c3f9cf0a81bdd6dec2787b26dd491e1c94433c62
Julia0602k <julia0602k@gmail.com>
2022-09-29 10:50:23 +0300

    task5_1 edit

--- 5_1.py
+++ 5_1.py
@@ -2,12 +2,10 @@
 n = int(input('Write n: '))
 m = int(input('Write m: '))
 k = int(input('Write k: '))
-k += 1
 number = 0
-list1 = []
 while number <= n:
+    k += 1
     if not k % m:
-        list1.append(k)
         number = number + 1
-        k *= m
- print(list1)
+        print(k)
+
