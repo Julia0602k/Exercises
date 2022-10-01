01f988ea1dd36bea1437b84b0e00d80f4b13e0cd
Julia0602k <julia0602k@gmail.com>
2022-10-01 12:50:58 +0300

    5_3 edit

--- 5_3.py
+++ 5_3.py
@@ -1,7 +1,7 @@
 #вывести четные числа от 2 до N по 5 в строкe
 n = int(input('Write n: '))
 for i in range(2, n + 1, 10):
-    for j in range (i, i+9, 2):
+    for j in range(i, i+9, 2):
         if j > n:
             break
         print(j, end=' ')
