45c31fc12ee3cf1c4a47e0132f1aaa998781b49e
Julia0602k <julia0602k@gmail.com>
2022-10-01 14:23:51 +0300

    1

--- 6_1.py
+++ 6_1.py
@@ -1,1 +1,5 @@
 #Написать функцию перевода десятичного числа в двоичное и обратно, без использования функции int
+number1 = int(input('Write a number(10): '))
+def bin_number(number1):
+
+bin_number()
