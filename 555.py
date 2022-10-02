d10ba67cdcc478a75100853b0f5526d9c4683c99
Julia0602k <julia0602k@gmail.com>
2022-10-02 11:53:08 +0300

    1111

--- 555.py
+++ 555.py
@@ -1,6 +1,9 @@
 n = int(input('Write N: '))
-list1 = []
-for i in range(n + 1):
-    if not i % 2:
-        list1.append(i ** 2)
+#list1 = []
+#for i in range(n + 1):
+#    if not i % 2:
+#        list1.append(i ** 2)
+#print(list1)
+
+list1 = [i ** 2 for i in range(2, n+1) if not i%2]
 print(list1)
