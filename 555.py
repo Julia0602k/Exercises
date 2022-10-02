25ba366da20c36ea492bb4fa7d1b0ce355d5cf0c
Julia0602k <julia0602k@gmail.com>
2022-10-02 12:26:06 +0300

    1111

--- 555.py
+++ 555.py
@@ -1,9 +1,22 @@
-n = int(input('Write N: '))
+#n = int(input('Write N: '))
+#1 variant
 #list1 = []
 #for i in range(n + 1):
 #    if not i % 2:
 #        list1.append(i ** 2)
 #print(list1)
 
-list1 = [i ** 2 for i in range(2, n+1) if not i%2]
-print(list1)
+#2 variant
+#list1 = [i ** 2 for i in range(2, n+1) if not i % 2]
+#print(list1)
+
+#n = input('Write a number')
+#while not n.isdigit():
+#    n = input('Write a number')
+#print('the end')
+
+text1 = input('Write a text: ')
+count.text1()
+
+
+
