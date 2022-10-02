2e0bd035818ed9049710d141d4e7b04ad78ea6e0
Julia0602k <julia0602k@gmail.com>
2022-10-02 17:30:34 +0300

    1111

--- 6_5.py
+++ 6_5.py
@@ -6,10 +6,12 @@ list1 = [1, 2, 3, 4, 5, 6, 7]
 def func_reverse1(list):
     n = int(len(list))
     i = 1
-    for i in range[::-1]:
+    for i in range(i, n+1, -1):
+        return list
         print(list)
 
 func_reverse1(list1)
+
 
 
 
