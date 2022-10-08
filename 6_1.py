d613aae95f087fcca036df79fad08c5b8b0cdaef
Julia0602k <julia0602k@gmail.com>
2022-10-08 16:18:23 +0300

    1111

--- 6_1.py
+++ 6_1.py
@@ -1,13 +1,24 @@
 #Написать функцию перевода десятичного числа в двоичное и обратно, без использования функции int
-number1 = int(input('Write number(in 10): '))
-def bin_number(number1) -> str:
+number1 = int(input('Write a number(in 10): '))
+def decimal_number_to_bin(number1) -> str:
     binary = ''
     while number1 > 1:
         binary += str(number1 % 2)
         number1 //= 2
     binary += str(number1)
     return binary[::-1]
-    print(binary)
 
-bin_number(number1)
+print (decimal_number_to_bin(number1))
+
+binary1 = input('Write a number (in bin): ')
+def bin_number_to_decimal(binary1: str) -> int:
+    number2 = 0
+    for i in binary1:
+        number2 *= 2
+        number2 += int(i)
+    return number2
+
+print(bin_number_to_decimal(binary1))
+
+
 
